83aeb46bfca18ee0330e97beb1e06fe17102a0da
Ben Gordon <bengg2015@gmail.com>
2020-11-28 12:31:51 -0500

    Fixed Size Issue
    Did it the hacky way, but oh well.

--- strip.lua
+++ strip.lua
@@ -1,11 +1,3 @@
-io.write("Size X: ")
-sizeX = tonumber(io.read())
-
-io.write("Size Z: ")
-sizeZ = tonumber(io.read())
-
-print("Fuel: " .. turtle.getFuelLevel())
-
 function mineFront2by1(z)
   turtle.dig()
   turtle.forward()
@@ -40,13 +32,28 @@ function goToNewRow(x)
   end
 end
 
--- MAIN LOOP:
-for x = 1, sizeX do
-  for z = 1, sizeZ do
-    mineFront2by1(z)
-  end
-  
-  if x < sizeX then
-    goToNewRow(x)
+function go()
+  for x = 1, sizeX do
+    for z = 1, sizeZ do
+      mineFront2by1(z)
+    end
+    
+    if x < sizeX then
+      goToNewRow(x)
+    end
   end
 end
+
+-- STARTING POINT:
+io.write("Size X: ")
+sizeX = tonumber(io.read())
+
+io.write("Size Z: ")
+sizeZ = tonumber(io.read()) - 1
+
+print("Fuel: " .. turtle.getFuelLevel())
+
+turtle.dig()
+turtle.forward()
+turtle.digUp()
+go()
