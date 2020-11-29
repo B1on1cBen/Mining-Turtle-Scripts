2f413538aafcb70c93456c935deb69bf8832d2ad
Ben Gordon <bengg2015@gmail.com>
2020-11-28 23:53:05 -0500

    Made Turtle Dump Shit and Resume
    Hooray!

--- strip.lua
+++ strip.lua
@@ -1,6 +1,5 @@
 function mineFront2by1(z)
-  turtle.dig()
-  turtle.forward()
+  DigAndMove()
 
   if z % 2 == 0 then
     turtle.digDown()
@@ -14,14 +13,12 @@ end
 function goToNewRow(x)
   if x % 2 == 0 then
     SetRotation(1)
-    turtle.dig()
-    turtle.forward()
+    DigAndMove()
     turtle.digUp()
     SetRotation(0)
   else
     SetRotation(1)
-    turtle.dig()
-    turtle.forward()
+    DigAndMove()
     turtle.digUp()
     SetRotation(2)
   end
@@ -32,16 +29,20 @@ function goToNewRow(x)
   end
 end
 
-function Go()
-  turtle.dig()
-  turtle.forward()
+function Mine()
+  DigAndMove()
   turtle.digUp()
 
   for x = 1, sizeX do
     for z = 1, sizeZ do
       mineFront2by1(z)
-      if CheckFuel() == false then
-        CheckShit()
+      if CheckFuel() == true or CheckShit() == true then
+        local resumePoint = vector.new(gps.locate(5))
+        local resumeDirection = direction
+        Go(home)
+        Refuel()
+        DumpShit()
+        Resume(resumePoint, resumeDirection)
       end
     end
     
@@ -65,9 +66,16 @@ function DigAndMove()
   end
 end
 
-function GoHome()
-  print("Going home.")
-  displacement = GetDisplacement()
+function Resume(resumePoint, resumeDirection)
+  SetRotation(0)
+  turtle.forward()
+  Go(resumePoint)
+  SetRotation(resumeDirection)
+end
+
+function Go(destination)
+  print("Going to " .. destination.x .. ", " .. destination.y .. ", " .. destination.z)
+  displacement = GetDisplacement(destination)
 
   SetRotation(3) -- West
   while displacement.x > 0 do
@@ -102,9 +110,6 @@ function GoHome()
     turtle.down()
     displacement.y = displacement.y + 1
   end
-
-  Refuel()
-  DumpShit()
 end
 
 function Refuel()
@@ -119,24 +124,25 @@ end
 function DumpShit()
   print("Dumping shit...")
   SetRotation(2)
+  local search = 0
 	for search = 16, 1, -1 do
 		turtle.select(search)
 		turtle.drop()
 	end
 end
 
-function GetDisplacement()
-  local location = vector.new(gps.locate(5))
-  print("Location: " .. location.x .. ", " .. location.y .. ", " .. location.z)
+function GetDisplacement(destination)
+  local turtlePos = vector.new(gps.locate(5))
+  print("Location: " .. turtlePos.x .. ", " .. turtlePos.y .. ", " .. turtlePos.z)
   
-  local displacement = home - location
+  local displacement = destination - turtlePos
   print("Displacement: " .. displacement.x .. ", " .. displacement.y .. ", " .. displacement.z .. "\n")
 
   return displacement
 end
 
 function CheckFuel()
-  local displacement = GetDisplacement()
+  local displacement = GetDisplacement(home)
   local displacementTotal = math.abs(displacement.x) + 
                             math.abs(displacement.y) + 
                             math.abs(displacement.z)
@@ -145,14 +151,40 @@ function CheckFuel()
 
   if homeTravelCostPercent >= 0.90 then
     print("Fuel low. Must refuel.\n")
-    GoHome()
     return true
   end
   return false
 end
 
 function CheckShit()
-
+  fullSlots = 0
+	local search = 0
+	for search = 16, 1, -1 do
+		if turtle.getItemCount(search) > 0 then
+			if tossShit == "yes" or tossShit == "y" or tossShit == "Y" then
+				if turtle.getItemDetail().name == "minecraft:cobblestone" or
+					 turtle.getItemDetail().name == "minecraft:stone" or
+					 turtle.getItemDetail().name == "minecraft:dirt" or
+					 turtle.getItemDetail().name == "minecraft:gravel" or
+					 turtle.getItemDetail().name == "chisel:marble2" or
+					 turtle.getItemDetail().name == "chisel:limestone2" or
+					 turtle.getItemDetail().name == "minecraft:netherrack" or
+					 turtle.getItemDetail().name == "natura:nether_tainted_soil" then
+            turtle.select(search)
+            turtle.drop()
+				end
+			end
+    end
+    
+    itemCount = turtle.getItemCount(search)
+		if itemCount > 0 then
+			fullSlots = fullSlots + 1
+		end
+	end
+	if fullSlots == 16 then
+		return true
+  end
+  return false
 end
 
 function SetRotation(rotation)
@@ -174,6 +206,9 @@ sizeX = tonumber(io.read())
 io.write("Size Z: ")
 sizeZ = tonumber(io.read()) - 1
 
+io.write("Toss Shit Y/N? ")
+tossShit = io.read()
+
 --[[
   North - 0
   East - 1
@@ -186,11 +221,4 @@ home = vector.new(gps.locate(5))
 print("Home: " .. home.x .. ", " .. home.y .. ", " .. home.z .. "\n")
 print("Fuel: " .. turtle.getFuelLevel())
 
---GoHome()
---Go()
-
-DigAndMove()
-DigAndMove()
-DigAndMove()
-DigAndMove()
-DigAndMove()
+Mine()
